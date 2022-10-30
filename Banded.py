43be4908b599b0dd03f7ae81664f7ac8d857d55e
Ryan Ogden <12yanogden@gmail.com>
2022-10-30 11:07:18 -0600

    add speedups

--- Banded.py
+++ Banded.py
@@ -1,12 +1,3 @@
-MATCH = -3
-INS_DEL = 5
-SUB = 1
-
-MAX_INS_DEL = 3
-BAND_RADIUS = MAX_INS_DEL + 1
-BAND_WIDTH = 2 * MAX_INS_DEL + 1
-
-
 class Banded:
     def __init__(self, seq1, seq2, align_length):
         self.seq1 = '-' + seq1
@@ -14,7 +5,7 @@ class Banded:
         self.seq1_length = len(seq1)
         self.seq2_length = len(seq2)
         self.align_length1 = min(self.seq1_length + 1, align_length + 1)
-        self.align_length2 = min(self.seq2_length + 1, align_length + 1, self.align_length1 + MAX_INS_DEL)
+        self.align_length2 = min(self.seq2_length + 1, align_length + 1, self.align_length1 + 3)
         self.scores = {}
         self.prevs = {}
 
@@ -23,77 +14,83 @@ class Banded:
     #                                         Align                                          #
     #                                                                                        #
     # ---------------------------------------------------------------------------------------#
-    def calc_diagonal_score(self, i, j):
-        diagonal_score = self.scores[i - 1, j - 1]
-        align1 = self.seq1[i]
-        align2 = self.seq2[j]
-
-        return diagonal_score + MATCH if align1 == align2 else diagonal_score + SUB
-
-    def calc_prevs(self, i, j):
-        prevs = [[self.calc_diagonal_score(i, j), 'D', 2]]
-
-        if (i + MAX_INS_DEL) != j:
-            prevs.append([self.scores[i - 1, j] + INS_DEL, 'U', 1])
-
-        if (i - MAX_INS_DEL) != j:
-            prevs.append([self.scores[i, j - 1] + INS_DEL, 'L', 0])
-
-        return prevs
-
-    def calc_min_prev(self, i, j):
-        score = 0
-        prev_char = 1
-        priority = 2
-        prevs = self.calc_prevs(i, j)
-
-        min_prev = prevs.pop()
-        for prev in prevs:
-            if prev[score] < min_prev[score] or \
-                    (prev[score] == min_prev[score] and prev[priority] < min_prev[priority]):
-                min_prev = prev
-
-        return min_prev[score], min_prev[prev_char]
-
-    def align_cell(self, i, j):
-        prev_score, prev_char = self.calc_min_prev(i, j)
-
-        self.scores[i, j] = prev_score
-        self.prevs[i, j] = prev_char
-
     def align(self):
+        second_third_section_border = self.align_length1 - 3
+
         # Align initial
         self.scores[0, 0] = 0
         self.prevs[0, 0] = None
 
         # Align edge scores
-        for i in range(1, BAND_RADIUS):
-            self.scores[i, 0] = i * INS_DEL
+        for i in range(1, 4):
+            self.scores[i, 0] = i * 5
             self.prevs[i, 0] = 'U'
-        for j in range(1, BAND_RADIUS):
-            self.scores[0, j] = j * INS_DEL
+        for j in range(1, 4):
+            self.scores[0, j] = j * 5
             self.prevs[0, j] = 'L'
 
         # Align first section
-        for i in range(1, BAND_RADIUS):
-            for j in range(1, BAND_RADIUS + i):
-                self.align_cell(i, j)
+        for i in range(1, 4):
+            for j in range(1, 4 + i):
+                diagonal_score = self.scores[i - 1, j - 1]
+                prevs = [[diagonal_score - 3 if self.seq1[i] == self.seq2[j] else diagonal_score + 1, 'D', 2]]
+
+                if (i + 3) != j:
+                    prevs.append([self.scores[i - 1, j] + 5, 'U', 1])
+
+                if (i - 3) != j:
+                    prevs.append([self.scores[i, j - 1] + 5, 'L', 0])
+
+                min_prev = prevs.pop()
+                for prev in prevs:
+                    if prev[0] < min_prev[0] or \
+                            (prev[0] == min_prev[0] and prev[2] < min_prev[2]):
+                        min_prev = prev
+
+                self.scores[i, j] = min_prev[0]
+                self.prevs[i, j] = min_prev[1]
 
         # Align second section
-        for i in range(BAND_RADIUS, self.align_length1 - MAX_INS_DEL):
-            for j in range(i - MAX_INS_DEL, i + BAND_RADIUS):
-                self.align_cell(i, j)
+        for i in range(4, second_third_section_border):
+            for j in range(i - 3, i + 4):
+                diagonal_score = self.scores[i - 1, j - 1]
+                prevs = [[diagonal_score - 3 if self.seq1[i] == self.seq2[j] else diagonal_score + 1, 'D', 2]]
 
-        if self.align_length2 >= self.align_length1 + MAX_INS_DEL:
-            # Continue to align second section
-            for i in range(self.align_length1 - MAX_INS_DEL, self.align_length1):
-                for j in range(i - MAX_INS_DEL, i + BAND_RADIUS):
-                    self.align_cell(i, j)
-        else:
-            # Align third section
-            for i in range(self.align_length1 - MAX_INS_DEL, self.align_length1):
-                for j in range(i - MAX_INS_DEL, self.align_length2):
-                    self.align_cell(i, j)
+                if (i + 3) != j:
+                    prevs.append([self.scores[i - 1, j] + 5, 'U', 1])
+
+                if (i - 3) != j:
+                    prevs.append([self.scores[i, j - 1] + 5, 'L', 0])
+
+                min_prev = prevs.pop()
+                for prev in prevs:
+                    if prev[0] < min_prev[0] or \
+                            (prev[0] == min_prev[0] and prev[2] < min_prev[2]):
+                        min_prev = prev
+
+                self.scores[i, j] = min_prev[0]
+                self.prevs[i, j] = min_prev[1]
+
+        # Align third section
+        for i in range(second_third_section_border, self.align_length1):
+            for j in range(i - 3, min(i + 4, self.align_length2)):
+                diagonal_score = self.scores[i - 1, j - 1]
+                prevs = [[diagonal_score - 3 if self.seq1[i] == self.seq2[j] else diagonal_score + 1, 'D', 2]]
+
+                if (i + 3) != j:
+                    prevs.append([self.scores[i - 1, j] + 5, 'U', 1])
+
+                if (i - 3) != j:
+                    prevs.append([self.scores[i, j - 1] + 5, 'L', 0])
+
+                min_prev = prevs.pop()
+                for prev in prevs:
+                    if prev[0] < min_prev[0] or \
+                            (prev[0] == min_prev[0] and prev[2] < min_prev[2]):
+                        min_prev = prev
+
+                self.scores[i, j] = min_prev[0]
+                self.prevs[i, j] = min_prev[1]
 
     # ---------------------------------------------------------------------------------------#
     #                                                                                        #
@@ -130,56 +127,3 @@ class Banded:
             curr = self.prevs[i, j]
 
         return alignment1, alignment2
-
-    # ---------------------------------------------------------------------------------------#
-    #                                                                                        #
-    #                                         Debug                                          #
-    #                                                                                        #
-    # ---------------------------------------------------------------------------------------#
-    def calc_top_row_string(self, initial_index2, end_index2):
-        out = " \t"
-
-        for j in range(initial_index2, end_index2):
-            out += self.seq2[j] + "\t"
-
-        out += "\n"
-
-        return out
-
-    def calc_string(self, initial_index1, end_index1, initial_index2, end_index2):
-        out = self.calc_top_row_string(initial_index2, end_index2)
-
-        # Print rest of table
-        for i in range(initial_index1, end_index1):
-
-            out += self.seq1[i] + "\t"
-
-            for j in range(initial_index2, end_index2):
-                cell = self.scores.get((i, j))
-
-                if cell is None:
-                    out += "-\t"
-                else:
-                    out += str(cell) + "\t"
-
-            out += "\n"
-
-        return out
-
-    def to_string(self):
-        out = ''
-
-        if self.align_length1 > 20 or self.align_length2 > 20:
-            out += "Initial\n"
-            out += self.calc_string(0, min(self.align_length1, 20),
-                                    0, min(self.align_length2, 20)) + "\n"
-
-            out += "End\n"
-            out += self.calc_string(self.align_length1 - 20, self.align_length1,
-                                    self.align_length2 - 20, self.align_length2)
-
-        else:
-            out += self.calc_string(0, self.align_length1,
-                                    0, self.align_length2)
-
-        return out.expandtabs(8)
--- Unrestricted.py
+++ Unrestricted.py
@@ -1,8 +1,3 @@
-MATCH = -3
-INS_DEL = 5
-SUB = 1
-
-
 class Unrestricted:
     def __init__(self, seq1, seq2, align_length):
         self.seq1 = '-' + seq1
@@ -19,29 +14,6 @@ class Unrestricted:
     #                                         Align                                          #
     #                                                                                        #
     # ---------------------------------------------------------------------------------------#
-    def calc_diagonal_score(self, i, j):
-        diagonal_score = self.scores[i - 1, j - 1]
-        align1 = self.seq1[i]
-        align2 = self.seq2[j]
-
-        return diagonal_score + MATCH if align1 == align2 else diagonal_score + SUB
-
-    def calc_min_score(self, i, j):
-        score = 0
-        prev_char = 1
-        priority = 2
-        prevs = [[self.calc_diagonal_score(i, j), 'D', 2],
-                 [self.scores[i - 1, j] + INS_DEL, 'U', 1],
-                 [self.scores[i, j - 1] + INS_DEL, 'L', 0]]
-
-        min_prev = prevs.pop()
-        for prev in prevs:
-            if prev[score] < min_prev[score] or \
-                    (prev[score] == min_prev[score] and prev[priority] < min_prev[priority]):
-                min_prev = prev
-
-        return min_prev[score], min_prev[prev_char]
-
     def align(self):
         # Initial score
         self.scores[0, 0] = 0
@@ -49,19 +21,28 @@ class Unrestricted:
 
         # Edge scores
         for i in range(1, self.align_length1):
-            self.scores[i, 0] = i * INS_DEL
+            self.scores[i, 0] = i * 5
             self.prevs[i, 0] = 'U'
         for j in range(1, self.align_length2):
-            self.scores[0, j] = j * INS_DEL
+            self.scores[0, j] = j * 5
             self.prevs[0, j] = 'L'
 
         # Fill in the rest of the scores
         for i in range(1, self.align_length1):
             for j in range(1, self.align_length2):
-                min_score, prev_char = self.calc_min_score(i, j)
+                diagonal_score = self.scores[i - 1, j - 1]
+                prevs = [[diagonal_score - 3 if self.seq1[i] == self.seq2[j] else diagonal_score + 1, 'D', 2],
+                         [self.scores[i - 1, j] + 5, 'U', 1],
+                         [self.scores[i, j - 1] + 5, 'L', 0]]
 
-                self.scores[i, j] = min_score
-                self.prevs[i, j] = prev_char
+                min_prev = prevs.pop()
+                for prev in prevs:
+                    if prev[0] < min_prev[0] or \
+                            (prev[0] == min_prev[0] and prev[2] < min_prev[2]):
+                        min_prev = prev
+
+                self.scores[i, j] = min_prev[0]
+                self.prevs[i, j] = min_prev[1]
 
     # ---------------------------------------------------------------------------------------#
     #                                                                                        #
@@ -98,56 +79,3 @@ class Unrestricted:
             curr = self.prevs[i, j]
 
         return alignment1, alignment2
-
-    # ---------------------------------------------------------------------------------------#
-    #                                                                                        #
-    #                                         Debug                                          #
-    #                                                                                        #
-    # ---------------------------------------------------------------------------------------#
-    def calc_top_row_string(self, initial_index2, end_index2):
-        out = " \t"
-
-        for j in range(initial_index2, end_index2):
-            out += self.seq2[j] + "\t"
-
-        out += "\n"
-
-        return out
-
-    def calc_string(self, initial_index1, end_index1, initial_index2, end_index2):
-        out = self.calc_top_row_string(initial_index2, end_index2)
-
-        # Print rest of table
-        for i in range(initial_index1, end_index1):
-
-            out += self.seq1[i] + "\t"
-
-            for j in range(initial_index2, end_index2):
-                cell = self.scores.get((i, j))
-
-                if cell is None:
-                    out += "-\t"
-                else:
-                    out += str(cell) + "\t"
-
-            out += "\n"
-
-        return out
-
-    def to_string(self):
-        out = ''
-
-        if self.align_length1 > 20 or self.align_length2 > 20:
-            out += "Initial\n"
-            out += self.calc_string(0, min(self.align_length1, 20),
-                                    0, min(self.align_length2, 20)) + "\n"
-
-            out += "End\n"
-            out += self.calc_string(self.align_length1 - 20, self.align_length1,
-                                    self.align_length2 - 20, self.align_length2)
-
-        else:
-            out += self.calc_string(0, self.align_length1,
-                                    0, self.align_length2)
-
-        return out.expandtabs(8)
